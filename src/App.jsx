68f4497d5f8ca8aa595a117a9c38c13fcba0acba
Sergey <sergsmile29@gmail.com>
2024-04-30 22:19:35 +0300

    init extra

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { ColorPicker } from './components/ColorPicker/ColorPicker'
-import { Counter } from './components/Counter/Counter'
 import ProductsApp from './components/ProuductsApp/ProductsApp'
-import { TodoList } from './components/TodoList/TodoList'
-import Vote from './components/Vote'
 
 const App = () => {
 	return (
 		<>
 			<ProductsApp />
-			{/* <Counter /> */}
-			{/* <ColorPicker /> */}
-			{/* <TodoList /> */}
-			{/* <Vote /> */}
 		</>
 	)
 }
