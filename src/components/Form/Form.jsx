6c1bcd00b369ed85d0b2fb73ddd3f30c5ecebe7d
Sergey <sergsmile29@gmail.com>
2024-05-02 22:51:52 +0300

    m3l2 init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -27,75 +27,15 @@ export const Form = ({ register }) => {
 		})
 	}
 
-	// DRY - Don't Repeat Yourself
-	// const handleChangeName = e => {
-	// 	setFormData({ ...formData, username: e.target.value })
-	// }
-	// const handleChangeEmail = e => {
-	// 	setFormData({ ...formData, email: e.target.value })
-	// }
-	// const handleChangePassword = e => {
-	// 	setFormData({ ...formData, password: e.target.value })
-	// }
-	// const user = {
-	// 	username: 'Vasya',
-	// 	email: 'XwLQK@example.com',
-	// 	job: {
-	// 		title: 'Programmer',
-	// 		company: 'Google',
-	// 		salary: 10000,
-	// 	},
-	// 	address: {
-	// 		city: 'Lviv',
-	// 		street: 'Shevchenka',
-	// 		house: 1,
-	// 	},
-	// }
-	// const { address, ...data } = user
-	// console.log('address', address)
-	// console.log('Rest', data)
-
-	// const fruits = ['apple', 'banana', 'cherry']
-
-	// const [firtsFruit, ...other] = fruits
-	// console.log(firtsFruit, other)
-
 	const handleChangeInput = e => {
 		const { name, value } = e.target
 		console.log(name, value)
 
-		// 1. If - Else
-		// if (name === 'username') {
-		// 	setFormData({ ...formData, username: value })
-		// }
-		// if (name === 'email') {
-		// 	setFormData({ ...formData, email: value })
-		// }
-		// if (name === 'password') {
-		// 	setFormData({ ...formData, password: value })
-		// }
-
-		// 2. Switch Case
-		// switch (name) {
-		// 	case 'username':
-		// 		setFormData({ ...formData, username: value })
-		// 		break
-		// 	case 'email':
-		// 		setFormData({ ...formData, email: value })
-		// 		break
-		// 	case 'age':
-		// 		setFormData({ ...formData, age: value })
-		// 		break
-		// 	case 'password':
-		// 		setFormData({ ...formData, password: value })
-		// }
-
-		// 3. Dynamic
 		if (name === 'terms') {
 			return setFormData({ ...formData, terms: !formData.terms })
 		}
-		// не піде далі після чекбоксу
-		setFormData({ ...formData, [name]: value })
+
+		setFormData(prev => ({ ...prev, [name]: value }))
 	}
 
 	return (
