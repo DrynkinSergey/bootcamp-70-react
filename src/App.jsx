af984c6548fd0a942cc62ca4f930b44b00672add
Sergey <sergsmile29@gmail.com>
2024-05-03 19:51:14 +0300

    clear project

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import { Employee } from './components/Employees/Employee'
-import { Form } from './components/Form/Form'
-import { ExampleForm } from './components/FormWithID/ExampleForm'
-import { PetForm } from './components/PetForm/PetForm'
-import { PetFormFormik } from './components/PetForm/PetFormFormik'
-import { PetFormRHF } from './components/PetForm/PetFormRHF'
-
-const App = () => {
-	const register = data => {
-		console.log('Sending data...')
-		setTimeout(() => {
-			console.log('Data sent', data)
-		}, 1000)
-		setTimeout(() => {
-			console.log(`Welcome on board, ${data.username}! 🔥`)
-		}, 2000)
-	}
+export const App = () => {
 	return (
-		<>
-			<PetFormFormik />
-			<PetFormRHF />
-		</>
+		<div>
+			<h1>React</h1>
+		</div>
 	)
 }
-
-export default App
