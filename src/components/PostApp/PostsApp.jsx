91fbdd2a4beaa7ccdebb519ef6d780ac96973de5
Sergey <sergsmile29@gmail.com>
2024-05-06 11:07:42 +0300

    intersection logic for infinity scroll

--- src/components/PostApp/PostsApp.jsx
+++ src/components/PostApp/PostsApp.jsx
@@ -1,26 +1,49 @@
 import { useEffect, useState } from 'react'
 import { List } from './List'
 import { fetchPosts } from '../../services/api'
+import Button from './../Button/Button'
+import { useInView } from 'react-intersection-observer'
 
 export const PostsApp = () => {
 	const [items, setItems] = useState([])
-
+	const [skip, setSkip] = useState(0)
+	const [limit, setLimit] = useState(5)
+	const [infinity, setInfinity] = useState(false)
+	const { ref, inView } = useInView({
+		threshold: 0,
+	})
 	useEffect(() => {
 		const getPosts = async () => {
 			try {
-				const { posts } = await fetchPosts()
-				setItems(posts)
+				const { posts } = await fetchPosts({ skip: skip })
+				setItems(prev => [...prev, ...posts])
 			} catch (error) {
 				console.log(error)
 			}
 		}
 
 		getPosts()
-	}, [])
+	}, [skip])
+
+	useEffect(() => {
+		infinity && setSkip(prev => prev + limit)
+	}, [inView, infinity, limit])
+
+	const handleChangeSkip = () => {
+		setSkip(prev => prev + limit)
+	}
 
 	return (
 		<div>
+			<h3>
+				Infinity scroll <Button onClick={() => setInfinity(prev => !prev)}>Enable</Button>
+			</h3>
 			<List items={items} />
+			{items.length ? (
+				<div style={{ visibility: infinity ? 'hidden' : 'visible' }} ref={ref}>
+					<Button onClick={handleChangeSkip}>Load more</Button>
+				</div>
+			) : null}
 		</div>
 	)
 }
--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,13 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'https://dummyjson.com/'
 
-export const fetchPosts = async () => {
-	const { data } = await axios.get('posts')
+export const fetchPosts = async configParams => {
+	// const { data } = await axios.get('posts?limit=5&page=${page}&api_key=hfk13hsdf4jasdfrh1312kdsladf&sort=desc&query=${query}') ❌
+	const { data } = await axios.get('posts', {
+		params: {
+			limit: 5,
+			...configParams,
+		},
+	})
 	return data
 }
