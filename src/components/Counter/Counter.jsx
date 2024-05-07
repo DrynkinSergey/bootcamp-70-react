cf357b93b85fe3352ea75c99d026c9ceb92865d4
Sergey <sergsmile29@gmail.com>
2024-05-07 11:02:58 +0300

    useMemo

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { render } from 'react-dom'
 
 export const Counter = () => {
 	const [counter, setCounter] = useState(1)
+	const [value, setValue] = useState(1)
 	const handleIncrement = () => {
 		setCounter(prev => prev + 1)
 	}
@@ -14,12 +15,21 @@ export const Counter = () => {
 		setCounter(0)
 	}
 
+	const calcData = () => {
+		console.log('Start calculation')
+		for (let i = 0; i < 1_000_000_000; i++) {
+			1 + 1
+		}
+		console.log('End calculation')
+		return value * value
+	}
+	const result = useMemo(calcData, [value])
 	const minusRef = useRef()
 	const myRef = useRef()
 	const renderCounter = useRef(0)
 	useEffect(() => {
-		myRef.current.focus()
-		myRef.current.innerHTML = 'HELLO WORLD'
+		// myRef.current.focus()
+		// myRef.current.innerHTML = 'HELLO WORLD'
 		// setInterval(() => {
 		// 	myRef.current.click()
 		// }, 1000)
@@ -36,6 +46,7 @@ export const Counter = () => {
 		<div className='min-h-screen flex justify-center items-center'>
 			<div className='border-2 border-black p-10 rounded-md flex flex-col gap-2'>
 				<h2 className='text-5xl text-center font-bold'>{counter}</h2>
+				<h2 className='text-5xl text-center font-bold'>{result}</h2>
 				<div className='flex gap-2 '>
 					<button ref={minusRef} onClick={handleDecrement} className='btn btn-outline btn-primary text-4xl'>
 						Minus
@@ -48,7 +59,7 @@ export const Counter = () => {
 						Plus
 					</button>
 				</div>
-				<button onClick={() => renderCounter.current++} id='btn' className='btn btn-outline btn-primary text-4xl'>
+				<button onClick={() => setValue(prev => prev + 1)} id='btn' className='btn btn-outline btn-primary text-4xl'>
 					REF BTN
 				</button>
 			</div>
