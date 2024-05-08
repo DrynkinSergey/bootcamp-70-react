455ac9d50a355a47bc6808967c584079b234055d
Sergey <sergsmile29@gmail.com>
2024-05-09 00:06:34 +0300

    init m5l1

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-import { Header } from './components/Header/Header'
-import { Login } from './components/Login'
-import { Cart } from './pages/Cart'
-import { Checkout } from './pages/Checkout'
-import { Home } from './pages/Home'
-import { Pizzas } from './pages/Pizzas'
-import { useAuth, usePages } from './store/hooks'
-
 export const App = () => {
-	const { page } = usePages()
-	const { isLoggedIn } = useAuth()
-
-	if (!isLoggedIn) {
-		return <Login />
-	}
-
-	return (
-		<div>
-			<Header />
-			{page === 'home' && <Home />}
-			{page === 'pizzas' && <Pizzas />}
-			{page === 'cart' && <Cart />}
-			{page === 'checkout' && <Checkout />}
-		</div>
-	)
+	return <div>Routing</div>
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,11 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
 import './index.css'
 import 'modern-normalize'
-import { PageProvider } from './store/PageProvider.jsx'
-import { AuthProvider } from './store/AuthProvider.jsx'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { PizzaProvider } from './store/PzzaProvider.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<PizzaProvider>
-		<AuthProvider>
-			<PageProvider>
-				<App />
-				<ToastContainer autoClose={1500} />
-			</PageProvider>
-		</AuthProvider>
-	</PizzaProvider>
+	<>
+		<App />
+		<ToastContainer autoClose={1500} />
+	</>
 )
