8dcaaf5c8fe39f50597d1a6b10ece92742deffd3
Sergey <sergsmile29@gmail.com>
2024-05-09 10:16:40 +0300

    add browserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import 'modern-normalize'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<>
+	// Обов'язкова обгортка для того, щоб ми могли парсити URL і працювала маршрутизація
+	<BrowserRouter>
 		<App />
 		<ToastContainer autoClose={1500} />
-	</>
+	</BrowserRouter>
 )
