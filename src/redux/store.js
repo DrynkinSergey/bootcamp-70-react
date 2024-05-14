2bf3ff4c543810ddd68cebd6aa99db67805cca91
Sergey <sergsmile29@gmail.com>
2024-05-14 10:52:12 +0300

    fix mode to uppercase

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { todoReducer } from './todolist/reducer'
 import { filterReducer } from './filter/reducer'
 import { configureStore } from '@reduxjs/toolkit'
 
+console.log(import.meta.env)
+
 export const store = configureStore({
 	reducer: {
 		counter: counterReducer,
 		todolist: todoReducer,
 		filter: filterReducer,
 	},
-	devTools: import.meta.env.mode !== 'production',
+	devTools: import.meta.env.MODE !== 'production',
 })
