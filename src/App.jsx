eb2e95e34726af3dbbe82d2cc64c36940d3327e3
Sergey <sergsmile29@gmail.com>
2024-05-16 00:04:06 +0300

    init m7l1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { Counter } from './components/Counter/Counter'
 import { TodolistApp } from './components/Todolist/TodolistApp'
-import { TrelloV2 } from './components/TrelloV2/TrelloV2'
 
 export const App = () => {
 	return (
 		<div>
-			{/* <Counter /> */}
-			{/* <TodolistApp /> */}
-			<TrelloV2 />
+			<TodolistApp />
 		</div>
 	)
 }
