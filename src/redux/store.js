34458baf94d587009abcb3beccd0ca2b027e94bb
Sergey <sergsmile29@gmail.com>
2024-05-17 11:06:17 +0300

    middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { todoReducer } from './todolist/slice'
 import { filterReducer } from './filter/slice'
+import logger from 'redux-logger'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+import moment from 'moment'
+
+const myMiddleware = store => next => action => {
+	if (action?.payload?.todo?.toLowerCase() === 'angular') {
+		axios.post('https://6645accbb8925626f892a498.mockapi.io/archive', {
+			createdAt: moment(new Date()).format('DD.MM.YYYY HH:mm:ss'),
+			...action.payload,
+		})
+	}
+	next(action)
+}
 
 export const store = configureStore({
 	reducer: {
 		todos: todoReducer,
 		filter: filterReducer,
 	},
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(myMiddleware),
 
 	devTools: import.meta.env.MODE !== 'production', // true
 })
