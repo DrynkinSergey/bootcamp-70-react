1a578bd5563b7edf7602ddde89aa9619d2297508
Sergey <sergsmile29@gmail.com>
2024-05-20 12:36:01 +0300

    navbar edit selector

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,17 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { NavLink } from 'react-router-dom'
-import { selectIsLoggedIn } from '../redux/auth/slice'
 import { logoutThunk } from '../redux/auth/operations'
 
 export const Navbar = () => {
 	const dispatch = useDispatch()
-	const isLoggedIn = useSelector(selectIsLoggedIn)
 	return (
 		<nav className='flex gap-4 items-center'>
 			<NavLink to='/'>Home</NavLink>
 			<NavLink to='/todos'>Todos</NavLink>
-			{!isLoggedIn ? (
-				<>
-					<NavLink to='/login'>Login</NavLink>
-					<NavLink to='/register'>Register</NavLink>
-				</>
-			) : (
-				<button onClick={() => dispatch(logoutThunk())} className='btn btn-primary'>
-					Logout
-				</button>
-			)}
+
+			<button onClick={() => dispatch(logoutThunk())} className='btn btn-primary'>
+				Logout
+			</button>
 		</nav>
 	)
 }
