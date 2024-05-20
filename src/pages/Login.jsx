c38fc5a1e18fb5e658fa983b9cf43b816fe246a4
Sergey <sergsmile29@gmail.com>
2024-05-20 10:56:26 +0300

    add register and login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,57 @@
+import { Field, Form, Formik } from 'formik'
+import { Link } from 'react-router-dom'
+
 export const Login = () => {
-	return <div>Login</div>
+	const initialValues = {
+		email: '',
+		password: '',
+	}
+	const handleSubmit = values => {
+		console.log(values)
+	}
+	return (
+		<div className='hero min-h-screen bg-base-200'>
+			<div className='hero-content flex-col lg:flex-row-reverse'>
+				<div className='text-center lg:text-left'>
+					<h1 className='text-5xl font-bold'>Login now!</h1>
+					<p className='py-6'>
+						Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+						deleniti eaque aut repudiandae et a id nisi.
+					</p>
+				</div>
+				<div className='card shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
+					<Formik initialValues={initialValues} onSubmit={handleSubmit}>
+						<Form className='card-body'>
+							<div className='form-control'>
+								<label className='label'>
+									<span className='label-text'>Email</span>
+								</label>
+								<Field type='email' name='email' placeholder='email' className='input input-bordered' required />
+							</div>
+							<div className='form-control'>
+								<label className='label'>
+									<span className='label-text'>Password</span>
+								</label>
+								<Field
+									type='password'
+									name='password'
+									placeholder='password'
+									className='input input-bordered'
+									required
+								/>
+								<label className='label text-sm'>
+									<Link to='/register'>Do not have account? Lets create it!</Link>
+								</label>
+							</div>
+							<div className='form-control mt-6'>
+								<button type='submit' className='btn btn-primary'>
+									Login
+								</button>
+							</div>
+						</Form>
+					</Formik>
+				</div>
+			</div>
+		</div>
+	)
 }
